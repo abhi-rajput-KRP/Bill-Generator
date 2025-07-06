22316fd3d332454a9647090fccd8b14d16583447
Abhi Rajput <Kartarsinghabhi2007@gmail.com>
2025-07-07 01:25:11 +0530

    Update dat.sql

--- dat.sql
+++ dat.sql
@@ -1,3 +1,3 @@
 create database bill;
-create table product(pro_name varchar(17) , price int(4),vendor varchar(20), quant_stock int(5));
-create table vendor(v_name varchar(20), price_due int(8));
+create table product(pro_name varchar(50) , price int(4),vendor varchar(50), quant_stock int(5));
+create table vendor(v_name varchar(50), price_due int(8));
